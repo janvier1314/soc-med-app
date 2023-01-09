94c8960a10d11b0c3ed33868aa6a373661bae1f4
janvier1314 <bhaskin13@gmail.com>
2023-01-10 03:01:47 +0800

    first commit

---
+++ src/lib/routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const ROOT = "/";
+
+export const router = createBrowserRouter([
+  {
+    path: ROOT,
+    element: "Public Root",
+  },
+]);
