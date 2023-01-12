f615260f0887e271343f8a3d5bd621d02b32bea4
janvier1314 <bhaskin13@gmail.com>
2023-01-12 23:36:24 +0800

    login functionality

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -10,10 +10,33 @@ import {
   Link,
   Text,
 } from "@chakra-ui/react";
-import { REGISTER } from "lib/routes";
+import { DASHBOARD, REGISTER } from "lib/routes";
 import { Link as RouterLink } from "react-router-dom";
+import { useLogin } from "hooks/auth";
+import { useForm } from "react-hook-form";
+import { emailValidate, passwordValidate } from "utils/form-validate";
 
 export default function Login() {
+  const { login, isLoading } = useLogin();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+
+  async function handleLogin(data) {
+    const succeeded = await login({
+      email: data.email,
+      password: data.password,
+      redirectTo: DASHBOARD, // this can be done straight here instead of in the hook
+    });
+
+    if (succeeded) reset();
+  }
+
   return (
     <Center w="100%" h="100vh">
       <Box mx="1" maxW="md" p="9" borderWidth="1px" borderRadius="lg">
@@ -21,14 +44,28 @@ export default function Login() {
           Log In
         </Heading>
 
-        <form onSubmit={() => {}}>
-          <FormControl isInvalid={true} py="2">
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <FormControl isInvalid={errors.email} py="2">
             <FormLabel>Email</FormLabel>
-            <Input type="email" placeholder="user@email.com" />
-            <FormErrorMessage>Please enter a valid email</FormErrorMessage>
-            <FormLabel>Password</FormLabel>
-            <Input type="password" placeholder="password" />
-            <FormErrorMessage>Incorrect password</FormErrorMessage>
+            <Input
+              type="email"
+              placeholder="user@email.com"
+              {...register("email", emailValidate)}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+            <FormControl isInvalid={errors.password} py="2">
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                placeholder="password"
+                {...register("password", passwordValidate)}
+              />
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
+            </FormControl>
           </FormControl>
           <Button
             mt="4"
@@ -36,7 +73,7 @@ export default function Login() {
             colorScheme="orange"
             size="md"
             w="full"
-            isLoading={true}
+            isLoading={isLoading}
             loadingText="Logging In"
           >
             Log In
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,16 +1,20 @@
 import { LOGIN } from "lib/routes";
 import { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "hooks/auth";
 
 export default function Layout() {
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const { user, isLoading } = useAuth();
 
   useEffect(() => {
-    if (pathname.startsWith("/protected")) {
+    if (pathname.startsWith("/protected") && !user) {
       navigate(LOGIN);
     }
-  }, [pathname]);
+  }, [pathname, user]);
+
+  if (isLoading) return "Loading...";
 
   return (
     <>
