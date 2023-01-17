6e4cf3339ee350f00183718375bd18d05fe1d4b7
janvier1314 <bhaskin13@gmail.com>
2023-01-17 23:47:56 +0800

    register

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,10 +1,14 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
-import { auth } from "lib/firebase";
+import { auth, db } from "lib/firebase";
 import { useState } from "react";
 import { DASHBOARD, LOGIN } from "lib/routes";
 import { useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { setDoc, doc } from "firebase/firestore";
 
 export function useAuth() {
   const [authUser, isLoading, error] = useAuthState(auth);
@@ -48,6 +52,46 @@ export function useLogin() {
   }
 
   return { login, isLoading };
+}
+
+export function useRegister() {
+  const [isLoading, setLoading] = useState(false);
+  const toast = useToast();
+
+  async function register({
+    username,
+    email,
+    password,
+    redirectTo = DASHBOARD,
+  }) {
+    setLoading(true);
+    const usernameExists = await isUsernameExists(username);
+
+    if (usernameExists) {
+      toast({
+        title: "Username already exists",
+        status: "error",
+        isClosable: true,
+        position: "top",
+        duration: 5000,
+      });
+      setLoading(false);
+    } else {
+      try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+
+        await setDoc(doc(db, "users", res.user.uid), {
+          id: res.user.uid,
+          username: username.toLowerCase(),
+          avatar: "",
+          date: Date.now(),
+        });
+      } catch (error) {}
+    }
+    setLoading(false);
+  }
+
+  return { register, isLoading };
 }
 
 export function useLogout() {
