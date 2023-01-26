1d44b5e027de4cac012cbd9fed6d8ec15aa464b2
janvier1314 <bhaskin13@gmail.com>
2023-01-26 18:51:37 +0800

    comments in actions - start

--- src/components/post/PostLists.js
+++ src/components/post/PostLists.js
@@ -3,7 +3,7 @@ import Post from "./index";
 
 export default function PostLists({ posts }) {
   return (
-    <Box px="4">
+    <Box align="center" px="4">
       {posts?.length === 0 ? (
         <Text textAlign="center" fontSize="xl">
           No posts yet...
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,12 +1,13 @@
 import { Box, Text } from "@chakra-ui/react";
+import Header from "./Header";
 
 export default function Post({ post }) {
-  const { text } = post;
+  const { uid, text, date } = post;
 
   return (
-    <Box p="2" maxW="600px">
+    <Box p="2" maxW="600px" textAlign="left">
       <Box border="2px solid" borderColor="gray.100" borderRadius="md">
-        {/* <Header /> */}
+        <Header uid={uid} date={date} />
 
         <Box p="2" minH="100px">
           <Text wordBreak="break-word" fontSize="md">
